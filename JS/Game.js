8a8c887a8e16aa02046f4b5269afce12d407d6e4
shivam26072006 <73473655+shivam26072006@users.noreply.github.com>
2021-02-02 19:57:29 +0530

    Add files via upload

--- JS/Game.js
+++ JS/Game.js
@@ -16,12 +16,26 @@ class Game{
         });
     }
 
-    start(){
+    async  start(){
         if(gameState === 0){
             player = new Player();
-            player.getCount();
-            form = new FormData();
+            var playerCountRef = await database.ref('playerCount').once("value");
+            if(playerCountRef.exists()){
+              playerCount = playerCountRef.val();
+              player.getCount();
+            }
+            form = new Form();
             form.display();
         }
+
+        car1 = createSprite(100,200);
+        car2 = createSprite(300,200);
+        car3 = createSprite(500,200);
+        car4 = createSprite(700,200);
+        cars = [car1,car2,car3,car4];
+
     }
+
+    
+
 }
--- sketch.js
+++ sketch.js
@@ -1,14 +1,25 @@
 var back, playerCount, database, gameState=0;
-var form, player, game;
+var form, player, game, allPlayers;
+var cars, car1, car2, car3, car4;
 
 function setup(){
 
-    createCanvas(600,600);
+    createCanvas(displayWidth-20, displayHeight-30);
     database = firebase.database();
+
+    game = new Game();
+    game.getState();
+    game.start();
 
 }
 
 function draw(){
 
-    
+    if(playerCount === 4){
+       game.update(1);
+    }
+    if(gameState === 1){
+        clear();
+        game.play();
+    }
 }
